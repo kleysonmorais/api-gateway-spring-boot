a60e3cd8677b36fe5a93cdb3a437b5b1e92e7bd8
Gabriel Lima <gabriellima.silva96@gmail.com>
2019-11-22 15:01:38 -0300

    Alterando o CORS para aceitar todos os métodos HTTP

--- src/main/java/com/validator/bpm/demo/DemoApplication.java
+++ src/main/java/com/validator/bpm/demo/DemoApplication.java
@@ -3,12 +3,14 @@ package com.validator.bpm.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
@@ -23,17 +25,14 @@ public class DemoApplication extends SpringBootServletInitializer {
   }
 
   @Bean
-  public FilterRegistrationBean<CorsFilter> corsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
+  public CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("*");
+    config.setAllowedOrigins(Collections.singletonList("*"));
+    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
     source.registerCorsConfiguration("/**", config);
-    FilterRegistrationBean<CorsFilter> bean =
-        new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-    bean.setOrder(-100000);
-    return bean;
+    return new CorsFilter(source);
   }
 }
